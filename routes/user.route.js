faba50cf91bed89485c75d7156867fb7225e05c4
lewis <thiengat1@gmail.com>
2020-05-17 05:21:25 -0700

    demo user

---
+++ routes/user.route.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var router = express.Router();
+
+var controller=require('../controllers/user.controller');
+
+router.get('/',controller.index);
+
+router.get('/search',controller.search);
+router.get('/create',controller.create);
+router.get('/:id', controller.get);
+router.post('/users/create',controller.postCreate);
+
+module.exports=router
